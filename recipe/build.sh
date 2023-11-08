f3dc071f944dcb624697898e639383c48499026a
Tobias Fischer <info@tobiasfischer.info>
2023-11-08 16:17:35 +1000

    Use newer cuda archs

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,22 +1,12 @@
 if [[ "$cuda_compiler_version" == "None" ]]; then
   export THESEUS_FORCE_CUDA=0
 else
-  if [[ ${cuda_compiler_version} == 9.0* ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;7.0+PTX"
-  elif [[ ${cuda_compiler_version} == 9.2* ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0+PTX"
-  elif [[ ${cuda_compiler_version} == 10.* ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5+PTX"
-  elif [[ ${cuda_compiler_version} == 11.0* ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0+PTX"
-  elif [[ ${cuda_compiler_version} == 11.1 ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
-  elif [[ ${cuda_compiler_version} == 11.2 ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6+PTX"
+  if [[ ${cuda_compiler_version} == 11.2 ]]; then
+    export TORCH_CUDA_ARCH_LIST="6.0;6.1;7.0;7.5;8.0;8.6+PTX"
   elif [[ ${cuda_compiler_version} == 11.8 ]]; then
-    export TORCH_CUDA_ARCH_LIST="3.5;5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9+PTX"
+    export TORCH_CUDA_ARCH_LIST="6.0;6.1;7.0;7.5;8.0;8.6;8.9+PTX"
   elif [[ ${cuda_compiler_version} == 12.0 ]]; then
-    export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
+    export TORCH_CUDA_ARCH_LIST="6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
   else
     echo "unsupported cuda version. edit build.sh"
     exit 1
