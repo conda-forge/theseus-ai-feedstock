311cee57ea71e6595c87a50668bb818525c0da65
Tobias Fischer <info@tobiasfischer.info>
2025-02-17 09:50:39 +1000

    Require compute >5.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,7 +13,7 @@ else
   elif [[ ${cuda_compiler_version} == 12.0 ]]; then
     export TORCH_CUDA_ARCH_LIST="6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
   elif [[ ${cuda_compiler_version} == 12.6 ]]; then
-    export TORCH_CUDA_ARCH_LIST="5.0;6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
+    export TORCH_CUDA_ARCH_LIST="6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
   else
     echo "unsupported cuda version. edit build.sh"
     exit 1
