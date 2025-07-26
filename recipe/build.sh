45908481c364168ddfca42c12c26074385752cae
Tobias Fischer <info@tobiasfischer.info>
2025-07-26 14:29:16 +1000

    add torch_cuda_arch_list for 12.9

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,6 +14,8 @@ else
     export TORCH_CUDA_ARCH_LIST="6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
   elif [[ ${cuda_compiler_version} == 12.6 ]]; then
     export TORCH_CUDA_ARCH_LIST="6.0;6.1;7.0;7.5;8.0;8.6;8.9;9.0+PTX"
+  elif [[ ${cuda_compiler_version} == 12.9 ]]; then
+    export TORCH_CUDA_ARCH_LIST="5.0;6.0;7.0;7.5;8.0;8.6;9.0;10.0;12.0+PTX"
   else
     echo "unsupported cuda version. edit build.sh"
     exit 1
